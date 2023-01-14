0ce7536ecc25153a089f7b4a0d41abc000f518f3
dom <domwilson95@hotmail.co.uk>
2023-01-14 22:33:49 +0000

    Renames and new exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { GameState } from "./state.js";
 export { PlayerBoard } from "./playerboard.js";
 export { printGame } from "./utils.js";
 export { PlayerInterface, PlayerType, Tile, Move } from "./azul.js";
-export { NegamaxAI, NegamaxAIOpts, NegamaxAIMode } from "./ai/players.js";
+export { AI, AIMode, AIOpts } from "./ai/players.js";
+export { PruningType } from "minimaxer";
