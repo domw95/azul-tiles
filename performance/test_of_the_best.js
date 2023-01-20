fef06be46e95775336564c83c58aa34be0668c82
dom <domwilson95@hotmail.co.uk>
2023-01-20 11:36:27 +0000

    Test best player

---
+++ performance/test_of_the_best.js
@@ -0,0 +1,16 @@
+import { PlayerCompare } from "../dist/ai/gameperf.js";
+import { AI, AIMode, AIOpts } from "../dist/ai/players.js";
+let opts = new AIOpts();
+opts.timeout = 100;
+opts.mode = AIMode.TIME;
+opts.clone = 1 /* CloneMethod.SMART */;
+opts.genBased = true;
+opts.presort = true;
+opts.eval = 0 /* EvalMethod.STANDARD */;
+const player0 = new AI(0, opts);
+opts = structuredClone(opts);
+opts.eval = 4 /* EvalMethod.LINE */;
+const player1 = new AI(1, opts);
+const players = [player0, player1];
+const pc = new PlayerCompare(players);
+pc.compare_alternate();
