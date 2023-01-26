74c4e63ab69ebf65ffd06c046d3f92bd16a53fc4
dom <domwilson95@hotmail.co.uk>
2023-01-26 15:17:54 +0000

    Profiling update

--- profiling/profile.ts
+++ profiling/profile.ts
@@ -6,33 +6,35 @@ const game = new GameState();
 game.newGame(2);
 
 const opts = new AIOpts();
-opts.timeout = 100;
-// opts0.print = true;
+opts.timeout = 10000;
+opts.print = true;
 opts.method = SearchMethod.TIME;
-// opts0.pruning = PruningType.ALPHA_BETA;
-// opts0.presort = true;
-// opts0.genBased = true;
+opts.pruning = PruningType.ALPHA_BETA;
+opts.presort = true;
+opts.genBased = true;
 const player = new AI(0, opts);
 
-for (let i = 0; ; i++) {
-    player.id = i % 2;
-    const move = player.getMove(game);
-    if (move != undefined) {
-        game.playMove(move);
-        if (!game.nextTurn()) {
-            if (!game.endRound()) {
-                break;
-            }
-        }
-    } else {
-        throw Error("Move undefined");
-    }
-}
+const move = player.getMove(game);
 
-// printGame(game);
-console.log(game.winner);
-console.log([
-    ...game.playerBoards.map((pb) => {
-        return pb.score;
-    }),
-]);
+// for (let i = 0; ; i++) {
+//     player.id = i % 2;
+//     const move = player.getMove(game);
+//     if (move != undefined) {
+//         game.playMove(move);
+//         if (!game.nextTurn()) {
+//             if (!game.endRound()) {
+//                 break;
+//             }
+//         }
+//     } else {
+//         throw Error("Move undefined");
+//     }
+// }
+
+// // printGame(game);
+// console.log(game.winner);
+// console.log([
+//     ...game.playerBoards.map((pb) => {
+//         return pb.score;
+//     }),
+// ]);
